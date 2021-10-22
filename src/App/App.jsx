636d3001db8a4eb837165a06a474c0e39029df54
Luke Laws <luke@antecursor.com>
2021-10-22 11:31:30 -0400

    Use rem for marginLeft  Sidebar

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -10,12 +10,12 @@ import { HeaderGlobalBar, HeaderGlobalAction } from 'carbon-components-react';
 import { SideNav, SideNavItems } from 'carbon-components-react';
 import { Dashboard16, VolumeUpFilled16, User24 } from '@carbon/icons-react';
 import { SkipToContent, SideNavLink } from 'carbon-components-react';
-import CampaignDetailComponent from '../Campaigns/CampaignDetail';
-import CampaignControlDetailComponent from '../Campaigns/CampaignControlDetail';
+import CampaignDetailComponent from '../campaigns/campaignDetail';
+import CampaignControlDetailComponent from '../campaigns/CampaignControlDetail';
 import EtfComponent from '../ETF/ETF';
 import { HomePage } from '../HomePage';
 import { OauthPage } from '../OauthPage/OauthPage'
-import { Login } from '../Login/Login';
+// import { Login } from '../login/login';
 
 
 function App (props) {
@@ -26,22 +26,18 @@ function App (props) {
     }
 
     const StoryContent = ({ useResponsiveOffset = true }) => {
-        const classNameFirstColumn = cx({
-          'bx--col': true,
-          'bx--offset-lg-3': useResponsiveOffset,
-        });
 
         const content = (
           <div className="bx--grid--full-width">
             <div className="bx--row" style={{'margin': '0'}}>
-              <div className={classNameFirstColumn} style={{padding: '0rem'}}>
-              <Router>
-                <Switch>
-                  <Route path='/' exact component={EtfComponent}/>  
-                  <Route path='/campaigns' exact component={CampaignDetailComponent}></Route>                  
-                  <Route path='/campaignscontroldetail' exact component={CampaignControlDetailComponent}></Route>                  
-                </Switch>
-              </Router>
+              <div className="bx--col" style={{padding: '0rem', marginLeft: '16rem'}}>
+                <Router>
+                  <Switch>
+                    <Route path='/' exact component={EtfComponent}/>  
+                    <Route path='/campaigns' exact component={CampaignDetailComponent}></Route>                  
+                    <Route path='/campaignscontroldetail' exact component={CampaignControlDetailComponent}></Route>                  
+                  </Switch>
+                </Router>
               </div>
             </div>
           </div>
@@ -84,22 +80,22 @@ function App (props) {
                     </HeaderGlobalAction>
                 </HeaderGlobalBar>
                 <SideNav aria-label="Side navigation" expanded={isSideNavExpanded}>
-                    <SideNavItems>
-                        <SideNavLink renderIcon={Dashboard16} href="/">
-                        ETFs
-                        </SideNavLink>
-                        <SideNavLink renderIcon={Dashboard16} href="/campaigns">
-                        Campaigns
-                        </SideNavLink>
-                        <SideNavLink renderIcon={Dashboard16} href="/campaignscontroldetail">
-                        Campaign Control Detail
-                        </SideNavLink>
-                        <SideNavLink renderIcon={VolumeUpFilled16} onClick={logout}>
-                        Logout
-                        </SideNavLink>
-                    </SideNavItems>
-                    </SideNav>
-                </Header>
+                  <SideNavItems>
+                    <SideNavLink renderIcon={Dashboard16} href="/">
+                    ETFs
+                    </SideNavLink>
+                    <SideNavLink renderIcon={Dashboard16} href="/campaigns">
+                    Campaigns
+                    </SideNavLink>
+                    <SideNavLink renderIcon={Dashboard16} href="/campaignscontroldetail">
+                    Campaign Control Detail
+                    </SideNavLink>
+                    <SideNavLink renderIcon={VolumeUpFilled16} onClick={logout}>
+                    Logout
+                    </SideNavLink>
+                  </SideNavItems>
+                </SideNav>
+              </Header>
             )}
             />
             <StoryContent>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import { App } from './App';
 import reportWebVitals from './reportWebVitals';
 import 'carbon-components/scss/globals/scss/styles.scss';
